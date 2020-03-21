81c64b331ea8366094f3f759260cac38e3e86421
s19600 <61684591+s19600@users.noreply.github.com>
2020-03-21 12:05:37 +0100

    v.0.0.3
    platforma znika ale nie pojawia się ponownie

---
+++ s19600_Praca_domowa_01/Assets/DisappearingPlatfom.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearingPlatfom : MonoBehaviour
+{
+    public float Time = 2;
+    Rigidbody2D rb;
+    
+
+    
+
+// Start is called before the first frame update
+void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+       if(collision.gameObject.name.Equals("Player"))
+        {
+            Destroy(gameObject,Time);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    
+}
+
