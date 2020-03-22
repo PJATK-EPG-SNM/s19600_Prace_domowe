eef69817745d8e649889c52fa6697d8f645d7ea0
s19600 <61684591+s19600@users.noreply.github.com>
2020-03-22 13:12:15 +0100

    v.0.04

---
+++ s19600_Praca_domowa_01/Assets/Falling_platform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falling_platform : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+ 
+    }
+
+     void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+            rb.isKinematic = false;
+    }
+
+    void OnCollisionEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        Debug.Log("You are dead");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
