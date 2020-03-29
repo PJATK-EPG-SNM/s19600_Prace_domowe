736ad2cc8582f39d125ec5d64764e5b730642ce1
s19600 <61684591+s19600@users.noreply.github.com>
2020-03-29 21:06:56 +0200

    v.0.0.2

---
+++ s19600_Praca_domowa_02/Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+
+{
+    public float speed = 500;
+    public float jump = 400;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isGrounded = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDisplacement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        rb.velocity = new Vector2(xDisplacement, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded==true))
+        {
+            rb.AddForce(new Vector2(0, jump));
+            isGrounded = false;
+
+        }
+    }
+
+     void OnCollisionEnter2D(Collision2D col)
+    {
+        isGrounded = true;
+    }
+}
